d1c9a07ab4ff372dc22a823d16388d67b7922399
Mohit Nayak <mohitn21@gmail.com>
2024-06-10 01:58:48 +0530

    homepage hero banner text fix

--- src/components/sections/hero-banner.jsx
+++ src/components/sections/hero-banner.jsx
@@ -10,15 +10,17 @@ const HeroBanner = ({
 }) => {
   return (
     <div className="relative h-[1248px] max-h-full overflow-hidden">
-      <StaticImage
-        src="../../assets/images/hero-banner-bg.png"
-        className="absolute top-0 h-full w-full"
-        formats={["png"]}
-        layout="fullWidth"
-        width={1440}
-        height={1248}
-        quality={100}
-      />
+      <div className="absolute top-0 h-full w-full">
+        <StaticImage
+          src="../../assets/images/hero-banner-bg.png"
+          formats={["png"]}
+          class="w-full h-full"
+          layout="fullWidth"
+          width={1440}
+          height={1248}
+          quality={100}
+        />
+      </div>
 
       <div className="relative mx-auto mt-44 max-w-3xl space-y-8 p-6 lg:px-8">
         <h1 className="text-title-primary-sm leading-title-primary-sm bg-primary-gradient lg:leading-title-primary-lg lg:text-title-primary-lg bg-clip-text text-center font-light text-transparent">
